94cd40ce9a7f75e9f095d14da2c176f302509531
MinaJi <minaaa0820@gmail.com>
2022-10-25 23:59:45 +0900

    auth create

--- react-firebase-crud/src/context/AuthContext.js
+++ react-firebase-crud/src/context/AuthContext.js
@@ -1,5 +1,7 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
+import { auth } from "../firebase-config";
 
 const INITIAL_STATE = {
   currentUser: JSON.parse(localStorage.getItem("user")) || null,
@@ -10,13 +12,21 @@ export const AuthContext = createContext(INITIAL_STATE);
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
+  const createUser = (email, password, nickname) => {
+    return createUserWithEmailAndPassword(auth, email, password, nickname);
+  };
+
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(state.currentUser));
   }, [state.currentUser]); // 새로고침해도 currentUser가 지워지지 않도록 로컬스토리지에 저장
 
   return (
-    <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
+    <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch, createUser }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+export const UserAuth = () => {
+    return UserContext(UserContext);
+}
--- react-firebase-crud/src/pages/SignUp.jsx
+++ react-firebase-crud/src/pages/SignUp.jsx
@@ -1,24 +1,61 @@
 import { Container, Grid } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase-config";
+import { async } from "@firebase/util";
 
 const StyledGrid = styled(Grid)`
   background-color: #ffffff;
   height: 600px;
   border-radius: 30px;
+  padding-top: 20px;
 `;
 
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+        await createUser
+    } catch (error) {
+      setError(error.message);
+      console.log(error.message);
+    }
+  };
+
   return (
-    <Container maxWidth="sm">
-      <Grid Container>
+    <Container maxWidth="sm" style={{ marginTop: "3%" }}>
+      <Grid container direction="column">
         <StyledGrid item>
-          회원가입
           <Grid item>
-            <input></input>
-            <input></input>
-            <input></input>
+            <p>Sign in</p>
           </Grid>
+          <form onSubmit={handleSubmit}>
+            <Grid item>
+              이메일{" "}
+              <input type="email" onChange={(e) => setEmail(e.target.value)} />
+            </Grid>
+            <Grid item>
+              비밀번호
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              닉네임
+              <input onChange={(e) => setNickname(e.target.value)} />
+            </Grid>
+            <Grid item>
+              <button type="submit">가입하기</button>
+            </Grid>
+          </form>
         </StyledGrid>
       </Grid>
     </Container>
